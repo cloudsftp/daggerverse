60fe0bb2113d5ccea3a19b65ce14cfc2ecb479d6
Fabian Weik <fabian.weik@protonmail.com>
2026-04-03 08:37:12 +0200

    add first unit test

--- merge-dirs/tests/main.go
+++ merge-dirs/tests/main.go
@@ -2,18 +2,65 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"dagger/tests/internal/dagger"
 )
 
 type Tests struct{}
 
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *Tests) GrepDir(ctx context.Context) error {
+func (m *Tests) TestMergeDirectories(ctx context.Context) error {
 	var err error
-	err = err
 
-	_ = dag.MergeDirs().MergeDirectories([]*dagger.Directory{})
+	dir1 := dag.Directory().
+		WithNewFile("a", "a").
+		WithNewFile("b", "b")
 
+	dir2 := dag.Directory().
+		WithNewFile("c", "c").
+		WithNewFile("d", "d")
+
+	merged := dag.MergeDirs().
+		MergeDirectories([]*dagger.Directory{
+			dir1,
+			dir2,
+		})
+
+	err = assertEntries(ctx, merged, []string{"a", "b", "c", "d"})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func assertEntries(
+	ctx context.Context,
+	directory *dagger.Directory,
+	expectedFiles []string,
+) error {
+	entries, err := directory.Entries(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get entries: %w", err)
+	}
+
+	expectedEntries := map[string]bool{}
+	for _, file := range expectedFiles {
+		expectedEntries[file] = false
+	}
+
+	for _, entry := range entries {
+		if _, exists := expectedEntries[entry]; exists {
+			expectedEntries[entry] = true
+		} else {
+			return fmt.Errorf("unexpected file in merged directory: %s", entry)
+		}
+	}
+
+	for file, found := range expectedEntries {
+		if !found {
+			return fmt.Errorf("expected file not found in merged directory: %s", file)
+		}
+	}
 	return nil
 }
