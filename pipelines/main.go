81a806f07b43b67487740ff9cad24e2bc6ce8b6a
Fabian Weik <fabian.weik@protonmail.com>
2026-04-14 21:03:25 +0200

    dagger develop before linting

--- pipelines/main.go
+++ pipelines/main.go
@@ -13,6 +13,10 @@ const (
 
 type CloudsDaggerModules struct{}
 
+func (m CloudsDaggerModules) Hello() string {
+	return "Hello, world!"
+}
+
 // Run the whole pipeline
 func (m CloudsDaggerModules) Run(
 	ctx context.Context,
