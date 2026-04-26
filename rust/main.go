586e9ce4c2adde791ace9aa308a26e2745287d29
Fabian Weik <fabian.weik@protonmail.com>
2026-04-26 09:15:14 +0200

    make rust components and installed packages configurable

--- rust/main.go
+++ rust/main.go
@@ -7,8 +7,14 @@ import (
 )
 
 type Rust struct {
-	RustVersion   string
+	// Version of rust to use
+	RustVersion string
+	// Version of alpine to use
 	AlpineVersion string
+	// Rust components to install in addition to rustfmt and clippy
+	Components []string
+	// Alpine packages to install in addition to pkgconfig and musl-dev
+	Packages []string
 }
 
 func New(
@@ -16,10 +22,19 @@ func New(
 	rustVersion string,
 	// +default="3.23"
 	alpineVersion string,
+	// +optional
+	components []string,
+	// +optional
+	packages []string,
 ) *Rust {
+	components = append(components, "rustfmt", "clippy")
+	packages = append(packages, "pkgconfig", "musl-dev")
+
 	return &Rust{
 		RustVersion:   rustVersion,
 		AlpineVersion: alpineVersion,
+		Components:    components,
+		Packages:      packages,
 	}
 }
 
@@ -27,14 +42,25 @@ func New(
 func (m *Rust) builder(source *dagger.Directory) *dagger.Container {
 	source = source.WithoutDirectory("target")
 
-	return dag.Container().
-		From(fmt.Sprintf("rust:%s-alpine%s", m.RustVersion, m.AlpineVersion)).
-		WithExec([]string{
-			"apk", "add", "--no-cache",
-			"pkgconfig", "musl-dev",
-			"openssl-dev", "openssl-libs-static",
-		}).
-		WithExec([]string{"rustup", "component", "add", "clippy"}).
+	builder := dag.Container().From(fmt.Sprintf("rust:%s-alpine%s", m.RustVersion, m.AlpineVersion))
+
+	if len(m.Packages) > 0 {
+		builder = builder.WithExec(append(
+			[]string{"apk", "add", "--no-cache"},
+			m.Packages...,
+		))
+	}
+
+	if len(m.Components) > 0 {
+		builder = builder.WithExec(append(
+			[]string{"rustup", "component", "add"},
+			m.Components...,
+		))
+
+	}
+
+	builder = builder.
+		// Source
 		WithDirectory("/src", source).
 		WithWorkdir("/src").
 
@@ -42,4 +68,6 @@ func (m *Rust) builder(source *dagger.Directory) *dagger.Container {
 		WithMountedCache("/cache/cargo", dag.CacheVolume("rust-packages")).
 		WithEnvVariable("CARGO_HOME", "/cache/cargo").
 		WithMountedCache("/src/target", dag.CacheVolume("rust-target"))
+
+	return builder
 }
