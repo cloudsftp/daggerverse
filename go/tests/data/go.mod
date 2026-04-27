e4c658f493c4b5c714093ee4e3f30723ef1f340c
Fabian Weik <fabian.weik@protonmail.com>
2026-04-27 20:12:26 +0200

    add tests for go runner

---
+++ go/tests/data/go.mod
@@ -0,0 +1,3 @@
+module github.com/cloudsftp/daggerverse/go-runner/tests/data
+
+go 1.26.1
---
+++ go/tests/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"dagger/go-tests/internal/dagger"
+)
+
+type GoTests struct{}
+
+// Run all go tests
+func (m *GoTests) Run(
+	ctx context.Context,
+	// +defaultPath="./data"
+	source *dagger.Directory,
+) error {
+	if err := m.TestLint(ctx, source); err != nil {
+		return err
+	}
+
+	if err := m.TestBuildExecutable(ctx, source); err != nil {
+		return err
+	}
+
+	if err := m.TestBuildImage(ctx, source); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Test linting go code
+func (m *GoTests) TestLint(
+	ctx context.Context,
+	// +defaultPath="./data"
+	source *dagger.Directory,
+) error {
+	err := dag.Go().
+		Lint(ctx, dagger.GoLintOpts{Source: source})
+	if err != nil {
+		return fmt.Errorf("could not lint: %w", err)
+	}
+
+	return nil
+}
+
+// Test building go executables
+func (m *GoTests) TestBuildExecutable(
+	ctx context.Context,
+	// +defaultPath="./data"
+	source *dagger.Directory,
+) error {
+	_, err := dag.Go().
+		Compile(dagger.GoCompileOpts{Source: source}).
+		Sync(ctx)
+	if err != nil {
+		return fmt.Errorf("could not build executable: %w", err)
+	}
+
+	return nil
+}
+
+// Test building the server image
+func (m *GoTests) TestBuildImage(
+	ctx context.Context,
+	// +defaultPath="./data"
+	source *dagger.Directory,
+) error {
+	_, err := dag.Go().
+		BuildImage(dagger.GoBuildImageOpts{Source: source}).
+		Sync(ctx)
+	if err != nil {
+		return fmt.Errorf("could not build image: %w", err)
+	}
+
+	return nil
+}
