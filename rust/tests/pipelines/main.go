7971052946591af7443b547877df6947a1abdb64
Clouds <me@cloudsftp.de>
2026-07-24 16:23:29 +0200

    remove rust service builder function

--- rust/tests/pipelines/main.go
+++ rust/tests/pipelines/main.go
@@ -31,10 +31,6 @@ func (m *RustTests) Run(
 		return err
 	}
 
-	if err := m.TestBuildImage(ctx, source); err != nil {
-		return err
-	}
-
 	return nil
 }
 
@@ -129,27 +125,6 @@ func (m *RustTests) TestBuild(
 
 	if _, err := dag.Rust().BuildExecutable(source, "bin-b").Sync(ctx); err == nil {
 		return fmt.Errorf("expected bin-b to fail build, but it succeeded")
-	}
-
-	return nil
-}
-
-// Test building service images
-func (m *RustTests) TestBuildImage(
-	ctx context.Context,
-	// +defaultPath="."
-	source *dagger.Directory,
-) error {
-	if _, err := dag.Rust().BuildImage(source, "root").Sync(ctx); err != nil {
-		return fmt.Errorf("expected root image to build: %w", err)
-	}
-
-	if _, err := dag.Rust().BuildImage(source, "bin-a").Sync(ctx); err != nil {
-		return fmt.Errorf("expected bin-a image to build: %w", err)
-	}
-
-	if _, err := dag.Rust().BuildImage(source, "bin-b").Sync(ctx); err == nil {
-		return fmt.Errorf("expected bin-b image to fail build, but it succeeded")
 	}
 
 	return nil
