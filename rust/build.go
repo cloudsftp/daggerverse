abcea9cbc74a83f1a4dbf6d9b83a7687cfb9f0cf
Clouds <me@cloudsftp.de>
2026-08-09 20:40:11 +0200

    make zigbuild default

--- rust/build.go
+++ rust/build.go
@@ -6,39 +6,24 @@ import (
 
 const resultDir = "/result"
 
-// Build a service executable with zig
-func (m *Rust) BuildExecutableZig(
-	source *dagger.Directory,
-	pkg string,
-	// +optional
-	target string,
-) *dagger.File {
-	buildCommand := []string{
-		"cargo", "zigbuild", "--release",
-		"-p", pkg,
-	}
-
-	if target != "" {
-		buildCommand = append(buildCommand, "--target", target)
-	}
-
-	return m.Builder(source).
-		WithExec([]string{"cargo", "install", "cargo-zigbuild"}).
-		WithExec(buildCommand).
-		WithDirectory(resultDir, dag.Directory()).
-		WithExec([]string{"cp", "target/release/" + pkg, resultDir}).
-		File(resultDir + "/" + pkg)
-}
-
 // Build a service executable
 func (m *Rust) BuildExecutable(
 	source *dagger.Directory,
 	pkg string,
 	// +optional
 	target string,
+	// +default=false
+	normal bool,
 ) *dagger.File {
+	buildVerb := ""
+	if normal {
+		buildVerb = "build"
+	} else {
+		buildVerb = "zigbuild"
+	}
+
 	buildCommand := []string{
-		"cargo", "build", "--release",
+		"cargo", buildVerb, "--release",
 		"-p", pkg,
 	}
 
